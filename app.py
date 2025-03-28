42e5dd89d806b6013476b594da7cb4255f0a6c85
dadakhalander <46879813+dadakhalander@users.noreply.github.com>
2025-03-28 12:59:47 +0100

    app.py

--- app.py
+++ app.py
@@ -10,24 +10,24 @@ def load_data():
 df = load_data()
 
 # Ensure the "Cluster" column exists before using it
-if "Cluster" not in df.columns:
-    st.error("Error: Column 'Cluster' not found in DataFrame")
+if "Cluster_k" not in df.columns:
+    st.error("Error: Column 'Cluster_k' not found in DataFrame")
 else:
     # Retrieve unique cluster labels
-    cluster_labels = sorted(df["Cluster"].unique())
+    cluster_labels = sorted(df["Cluster_k"].unique())
 
     # Sidebar for cluster selection
-    selected_cluster = st.sidebar.selectbox("Select Cluster", cluster_labels)
+    selected_cluster = st.sidebar.selectbox("Select Cluster_k", cluster_labels)
 
     # Filter data for the selected cluster
-    cluster_data = df[df["Cluster"] == selected_cluster]
+    cluster_data = df[df["Cluster_k"] == selected_cluster]
 
     # Set colors for the clusters
     cluster_colors = {label: px.colors.qualitative.Set1[i] for i, label in enumerate(cluster_labels)}
     color = cluster_colors[selected_cluster]
 
     # Set page title
-    st.title(f"Cluster {selected_cluster} Analysis")
+    st.title(f"Cluster_k {selected_cluster} Analysis")
 
     # Plot histogram of age distribution based on gender
     fig1 = px.histogram(cluster_data, x="Age_original", color="Gender_Male",
@@ -37,5 +37,5 @@ else:
     st.plotly_chart(fig1)
 
     # Display cluster statistics
-    st.subheader(f"Cluster {selected_cluster} Statistics")
+    st.subheader(f"Cluster_k {selected_cluster} Statistics")
     st.write(cluster_data[['Age_original', 'Annual_Income (£K)_original', 'Spending_Score_original']].describe())
